53c50e0b49e21ce26b58323c61a2a534f696257e
Abhishek anand <abhishekkumar85929@gmail.com>
2026-09-21 08:44:19 +0530

    Initial commit

---
+++ React.js with S. sharma/my-app/script.js
@@ -0,0 +1,4 @@
+var h1 = document.createElement('h1')
+h1.innerHTML = 'Hello form JS'
+
+document.body.appendChild(h1)
---
+++ React.js with S. sharma/my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>ABhi Web Application
+      <h1>Hello World</h1>
+      <h2>Wellcome To My Website</h2>
+      <h3>I Wish We will Liked My Website</h3>
+    </div>
+  )
+}
+
+export default App
